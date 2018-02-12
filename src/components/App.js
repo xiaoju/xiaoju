74d16f59ff5b9cda4e8a24a9be7b076e98025eca
xiaoju <me@xiaoju.io>
2018-02-13 00:23:44 +0100

    feat: flexbox layout including navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 
+import NavigationBar from './NavigationBar';
 import Home from './Home';
 import Home0 from './Home0';
 import Responsive from './Responsive';
 import Bootstrap from './Bootstrap';
 import Airbnb from './Airbnb';
 import Sandbox from './Sandbox';
-import Flexbox from './Flexbox';
 import PageNotFound from './PageNotFound';
 
 function App(props) {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/Responsive" component={Responsive} />
-        <Route path="/bootstrap" component={Bootstrap} />
-        <Route path="/sandbox" component={Sandbox} />
-        <Route path="/flexbox" component={Flexbox} />
-        <Route path="/airbnb" component={Airbnb} />
-        <Route component={PageNotFound} />
-      </Switch>
+    <div
+      style={{
+        height: '100%'
+      }}
+    >
+      <NavigationBar />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          height: '100%'
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/Responsive" component={Responsive} />
+          <Route path="/bootstrap" component={Bootstrap} />
+          <Route path="/sandbox" component={Sandbox} />
+          <Route path="/airbnb" component={Airbnb} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
     </div>
   );
 }
