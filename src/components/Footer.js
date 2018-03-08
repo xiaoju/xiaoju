af8fa5f3efea7c1f3201e450e727bac9d435bab7
xiaoju <me@xiaoju.io>
2018-03-08 11:40:39 +0100

    feat: add tooltips to footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import iconLn from '../pictures/iconLn.svg';
 import iconLn_hover from '../pictures/iconLn_hover.svg';
 import iconEnvelope from '../pictures/iconEnvelope.svg';
@@ -11,73 +12,99 @@ import iconGps_hover from '../pictures/iconGps_hover.svg';
 function Footer() {
   return (
     <div id="footer" className="xj-footer">
-      <a href="https://goo.gl/maps/apLJXjbFjfm" className="logo_swapping left">
-        <img
-          className="logo_passive"
-          src={iconGps}
-          height="35"
-          title="Toulouse, France"
-          alt="location: Toulouse, France"
-        />
-        <img
-          className="logo_hover"
-          src={iconGps_hover}
-          height="35"
-          title="Toulouse, France"
-          alt="location: Toulouse, France"
-        />
-      </a>
-      <a href="http://www.linkedin.com/in/sourcing" className="logo_swapping">
-        <img
-          className="logo_hover"
-          src={iconLn_hover}
-          height="35"
-          alt="linkedInlogo"
-          title="linkedin.com/in/sourcing/"
-        />
-        <img
-          className="logo_passive"
-          src={iconLn}
-          height="35"
-          alt="linkedInlogo"
-          title="https://www.linkedin.com/in/sourcing"
-        />
-      </a>
-      <a
-        href="mailto:jerome@xiaoju.io?subject=xiaoju.io - contact&body=Hi Jerome,"
-        className="logo_swapping"
+      <OverlayTrigger
+        overlay={<Tooltip id="location">{'Toulouse, France'}</Tooltip>}
+        placement="top"
+        delayShow={150}
+        delayHide={80}
       >
-        <img
-          className="logo_passive"
-          src={iconEnvelope}
-          height="35"
-          title="jerome@xiaoju.io"
-          alt="email: jerome@xiaoju.io"
-        />
-        <img
-          className="logo_hover"
-          src={iconEnvelope_hover}
-          height="35"
-          title="jerome@xiaoju.io"
-          alt="email: jerome@xiaoju.io"
-        />
-      </a>
-      <a href="https://github.com/xiaoju" className="logo_swapping">
-        <img
-          className="logo_hover"
-          src={iconGithub_hover}
-          height="35"
-          title="github.com/xiaoju"
-          alt="github.com/xiaoju"
-        />
-        <img
-          className="logo_passive"
-          src={iconGithub}
-          height="35"
-          title="github.com/xiaoju"
-          alt="github.com/xiaoju"
-        />
-      </a>
+        <a
+          className="logo_swapping left"
+          href="https://goo.gl/maps/apLJXjbFjfm"
+        >
+          <img
+            className="logo_passive"
+            src={iconGps}
+            height="35"
+            alt="location: Toulouse, France"
+          />
+          <img
+            className="logo_hover"
+            src={iconGps_hover}
+            height="35"
+            alt="location: Toulouse, France"
+          />
+        </a>
+      </OverlayTrigger>
+      <OverlayTrigger
+        overlay={<Tooltip id="location">{'linkedin.com/in/sourcing'}</Tooltip>}
+        placement="top"
+        delayShow={150}
+        delayHide={80}
+      >
+        <a
+          className="logo_swapping"
+          href="https://www.linkedin.com/in/sourcing"
+        >
+          <img
+            className="logo_passive"
+            src={iconLn}
+            height="35"
+            alt="linkedin.com/in/sourcing"
+          />
+          <img
+            className="logo_hover"
+            src={iconLn_hover}
+            height="35"
+            alt="linkedin.com/in/sourcing"
+          />
+        </a>
+      </OverlayTrigger>
+      <OverlayTrigger
+        overlay={<Tooltip id="location">{'jerome@xiaoju.io'}</Tooltip>}
+        placement="top"
+        delayShow={150}
+        delayHide={80}
+      >
+        <a
+          className="logo_swapping"
+          href="mailto:jerome@xiaoju.io?subject=xiaoju.io - contact&body=Hi Jerome, "
+        >
+          <img
+            className="logo_passive"
+            src={iconEnvelope}
+            height="35"
+            alt="jerome@xiaoju.io"
+          />
+          <img
+            className="logo_hover"
+            src={iconEnvelope_hover}
+            height="35"
+            alt="jerome@xiaoju.io"
+          />
+        </a>
+      </OverlayTrigger>
+      <OverlayTrigger
+        overlay={<Tooltip id="location">{'github.com/xiaoju'}</Tooltip>}
+        placement="top"
+        delayShow={150}
+        delayHide={80}
+      >
+        <a className="logo_swapping" href="https://github.com/xiaoju">
+          <img
+            className="logo_passive"
+            src={iconGithub}
+            height="35"
+            alt="github.com/xiaoju"
+          />
+          <img
+            className="logo_hover"
+            src={iconGithub_hover}
+            height="35"
+            alt="github.com/xiaoju"
+          />
+        </a>
+      </OverlayTrigger>
     </div>
   );
 }
